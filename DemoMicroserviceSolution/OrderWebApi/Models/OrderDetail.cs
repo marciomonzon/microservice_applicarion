6e982061deae89cde34f21da9deaac7c2a2fa701
marciomonzon <marcio.pcmonzon@gmail.com>
2022-10-31 19:32:38 -0300

    order api

---
+++ DemoMicroserviceSolution/OrderWebApi/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OrderWebApi.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class OrderDetail
+    {
+        [BsonId, BsonElement("product_id"), BsonRepresentation(BsonType.Int32)]
+        public int ProductId { get; set; }
+
+        [BsonId, BsonElement("quantity"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal Quantity { get; set; }
+
+        [BsonId, BsonElement("unity_price"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal UnitPrice { get; set; }
+    }
+}
